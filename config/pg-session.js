dca695ec7774c6ccedb769c0465ff92717bd1a91
cgomezlop95 <c.gomezlop95@gmail.com>
2023-12-20 21:11:44 +0100

    updated env

--- config/pg-session.js
+++ config/pg-session.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 
 const pgPool = new pg.Pool({
   user: process.env.POSTGRES_USER,
-  host: "localhost",
-  database: "postgres",
+  host: process.env.POSTGRES_HOST || "localhost",
+  database: process.env.POSTGRES_DATABASE_NAME,
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
 });
