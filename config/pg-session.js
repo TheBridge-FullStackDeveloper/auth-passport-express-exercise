903404a4c584356e18488b6390948df83a146475
cgomezlop95 <c.gomezlop95@gmail.com>
2023-12-21 11:57:40 +0100

    updated pg

--- config/pg-session.js
+++ config/pg-session.js
@@ -9,6 +9,7 @@ const pgPool = new pg.Pool({
   database: process.env.POSTGRES_DATABASE_NAME,
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
+  ssl: true,
 });
 
 module.exports = pgPool;
